3232f7034e3b35def146de248c85c5db5d144a03
aki05162525 <akihiro05162525@gmail.com>
2025-11-08 19:28:05 +0900

    usersテーブルのみ追加

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   out: './drizzle',
-  schema: './lib/db/shema',
+  schema: './db/shema',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
